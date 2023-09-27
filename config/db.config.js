5514b2a758939573bb9bcde792ca57338e6a2e2e
p <peenp1234@gmail.com>
2023-09-27 16:06:43 +0700

    add api

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,15 @@
+require(`dotenv`).config();
+
 module.exports ={
-    HOST:"localhost",
-    USER:"root",
-    PASSWORD:"",
-    DB :"restaurant"
+    HOST:process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB,
+    dialect:"postgres",
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    }
 }
--- server.js
+++ server.js
@@ -3,6 +3,35 @@ const cors = require("cors");
 const sql = require("./models/db");
 const PORT =5000;
 const restaurantRouter = require("./routes/restaurant.router")
+const req = require("express/lib/request")
+const db = require("./models/index")
+const role = db.role
+
+
+
+
+
+///dev mode
+db.sequelize.sync({force: true}).then(() =>{
+    console.log("Drop and resync DB");
+    initial();
+})
+
+function initial() {
+    role.create9({
+        id:1,
+        name:"user",
+    });
+     role.create9({
+         id: 2,
+         name: "moderrator",
+     });
+      role.create9({
+          id: 3,
+          name: "admin",
+      });
+}
+
 
 //creat service
 const app = express();
@@ -13,7 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.get("/", (req,res)=>{
-    res.send("<h1>Hello</h1>");
+    res.send("<h1>Restaurant</h1>");
 })
 app.use("/",restaurantRouter)
 
